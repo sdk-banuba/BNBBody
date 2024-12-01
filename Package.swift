a7086f538c0f1045c705af5813dc19439902c2b5
ci <ci@banuba.com>
2024-12-02 00:07:58 +0300

    1.14.1-211-g9ce64ef1c2

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-203-g2a72d3010f"
+let bnbPackageVersion: Version = "1.14.1-211-g9ce64ef1c2"
 
 let package = Package(
     name: "BNBBody",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBBody",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-203-g2a72d3010f/BNBBody.zip",
-            checksum: "a96e5bc4ba8cf59312c0f5acdb4f039c19312cef60290e96d8a1a649eb295598"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-211-g9ce64ef1c2/BNBBody.zip",
+            checksum: "9ba5f43b22cd19d73803849e0b031c8edfe85b2949b3ce001811d1835eff6ad2"
         ),
         .target(
             name: "BNBBody_BNBSdkCore",
